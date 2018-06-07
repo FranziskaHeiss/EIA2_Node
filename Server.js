feee73c897cc6373c6a105b0b6ef68bc55d0f466
Franziska <Franziska@PC1996.eduroam.hs-furtwangen.de>
2018-06-07 11:58:33 +0200

    Server.ts geändert Z.41/45

--- Server.js
+++ Server.js
@@ -15,10 +15,10 @@ var Server;
     if (port == undefined)
         port = 8100;
     let server = Http.createServer();
-    server.addListener("listening", handleListen);
+    server.addListener("request", handleRequestSetHeader);
     server.addListener("request", handleRequest);
     server.listen(port);
-    function handleListen(_request, _response) {
+    function handleRequestSetHeader(_request, _response) {
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
     }
